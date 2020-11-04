e8e9498a4b01141adf091072f5d3e8c89573c79e
Burak Çiçek <burakcicek@Burak-MacBook-Pro.local>
2020-11-04 14:25:19 +0300

    modified gitignore to stop tracking Constants.swift, added ConstantsTemplate.swift istead

---
+++ youtubeTry/ConstantsTemplate.swift
@@ -0,0 +1,22 @@
+//
+//  ConstantsTemplate.swift
+//  youtubeTry
+//
+//  Created by Burak Çiçek on 4.11.2020.
+//
+
+/*
+ Add your own keys below and uncomment the code
+ */
+
+import Foundation
+
+/*
+struct Constants {
+    
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
+    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    
+}
+*/
